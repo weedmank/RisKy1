0f2f2a5cf683ece6fa3a752972221fe1c3be2a70
Kirk Weedman <kirk@hdlexpress.com>
2020-11-08 09:44:11 -0800

    updated license header for all source files. They all all the same/consistent now

--- design/src/includes/csr_params.svh
+++ design/src/includes/csr_params.svh
@@ -1,8 +1,13 @@
 // ----------------------------------------------------------------------------------------------------
-// Creative Commons - Attribution - ShareAlike 3.0
 // Copyright (c) 2020 Kirk Weedman www.hdlexpress.com
-// Notice: For any reuse or distribution, you must make clear to others the license terms of this work.
-// see http://creativecommons.org/licenses/by/3.0/
+// Copyright and related rights are licensed under the Solderpad Hardware
+// License, Version 0.51 (the "License"); you may not use this file except in
+// compliance with the License.  You may obtain a copy of the License at
+// http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
+// or agreed to in writing, software, hardware and materials distributed under
+// this License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
 // ----------------------------------------------------------------------------------------------------
 // Project       :  RisKy1 - new 5 stage pipelined RISC-V ISA based CPU tailored to the RISC-V RV32IM
 // Editor        :  Notepad++
